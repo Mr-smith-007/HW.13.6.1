12d6e23edd1c33d9046cd2071138b623c85c17cb
Repnikov.A <repnikov.a@florcom.ru>
2023-09-19 14:36:35 +0300

    Списки List и LinkedList

--- HW.13.6.1/Program.cs
+++ HW.13.6.1/Program.cs
@@ -4,33 +4,43 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
-namespace Project_13._6._2
+
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<string> listWords = new List<string>();
+        LinkedList<string> linkedListWords = new LinkedList<string>();
+        string[] words;
+
+        string path = @"C:\Users\repnikov.a\Desktop\Text_13_6_1.txt";
+        using (var sr = new StreamReader(path))
         {
-            LinkedList<string> linkedListWords = new LinkedList<string>();
-            string[] words;
+            var text = sr.ReadToEnd().ToLower();
 
-            string path = @"C:\Users\repnikov.a\Desktop\Text_13_6_1.txt";
-            using (var sr = new StreamReader(path))
-            {
-                var text = sr.ReadToEnd().ToLower();
+            text = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
 
-                text = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
-
-                words = text.Split(new char[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-
-            }
-            linkedListWords.AddFirst("1");
-            var watch = Stopwatch.StartNew();
-            foreach (var word in words)
-                linkedListWords.AddAfter(linkedListWords.First, word);
-            Console.WriteLine($"Вставка в LinkedList<T> заняла: {watch.Elapsed.TotalMilliseconds} мс");
-
-            Console.ReadKey();
+            words = text.Split(new char[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
         }
 
+
+        var watch1 = Stopwatch.StartNew();
+        foreach (var word in words)
+            listWords.Add(word);
+        Console.WriteLine($"Вставка в List<T> заняла: {watch1.Elapsed.TotalMilliseconds} мс");
+
+
+        Console.WriteLine();
+
+
+        linkedListWords.AddFirst("1");
+        var watch2 = Stopwatch.StartNew();
+        foreach (var word in words)
+            linkedListWords.AddAfter(linkedListWords.First, word);
+        Console.WriteLine($"Вставка в LinkedList<T> заняла: {watch2.Elapsed.TotalMilliseconds} мс");
+
+        Console.ReadKey();
     }
+
 }
